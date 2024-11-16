c51992c9406a9ebc2d46bdec9dc61005d6e29c3e
MAxlekansder <alexander.hasslund@stud.sti.se>
2024-11-16 17:07:33 +0100

    already work in progress

---
+++ src/test/java/org/example/simplerpgateway/SimplErpGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.simplerpgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplErpGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
